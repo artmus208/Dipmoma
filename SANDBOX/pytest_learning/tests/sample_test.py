9e463460ddcfd7e46d5a3f98a50305b4a958a041
Artur Mustafin <mustafinrtz.2000@gmail.com>
2023-05-25 11:30:19 +0300

    Wiped Out

--- SANDBOX/pytest_learning/tests/sample_test.py
+++
@@ -1,5 +0,0 @@
-def foo(a, b):
-    return a + b
-
-def test_foo():
-    assert foo(3, 2) == 5
--- app/config.py
+++
@@ -1,9 +0,0 @@
-import os
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-class Config:
-    SECRET_KEY = '8b2771f9bdfgdfgw186123sdfsf749ccadad43'
-    MAX_CONTENT_LENGTH = 16*1024*1024
-    UPLOADED_TEXT_DEST = os.path.join(basedir, 'static/data/uploads')
-    UPLOADS_DEFAULT_DEST = os.path.join(basedir, 'static/data/uploads')
-    ALLOWED_EXTENSIONS = {'txt'}
